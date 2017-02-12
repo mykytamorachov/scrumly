1591cbdfa2e8f24fea0129716e64550b31dc5006
mykytamorachov <siner08@gmail.com>
2017-02-12 14:08:07 +0100

    added sprint

---
+++ src/app/services/sprint.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+import { Sprint } from '../classes/sprint'
+import { SprintType } from '../types/sprint-type'
+
+@Injectable()
+export class SprintService {
+  constructor() { }
+}
